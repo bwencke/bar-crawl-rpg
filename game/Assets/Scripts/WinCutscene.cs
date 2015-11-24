0e7c6879f3900c4271539e1c3b54a1613dcae9bd
Ben Wencke <benwencke@gmail.com>
2015-11-23 19:34:54 -0500

    Ending

--- game/Assets/Scripts/WinCutscene.cs
+++ game/Assets/Scripts/WinCutscene.cs
@@ -7,11 +7,22 @@ public class WinCutscene : CutsceneScript {
 	int numScenes = 1;
 
 	public GameObject canvas;
+	public GameObject thankYou;
+	public AudioObject barAudio;
 
 	public override IEnumerator Next (System.Action callback) {
 		switch (position++) {
 		case(0):
+			barAudio.StopAudio();
 			canvas.GetComponent<Canvas> ().enabled = true;
+			GameObject.FindGameObjectWithTag("WinParticles").GetComponent<ParticleSystem>().Play();
+			canvas.GetComponentInChildren<AudioObject>().PlayAudio();
+			yield return new WaitForSeconds(6);
+			GameObject.FindGameObjectWithTag("WinParticles").GetComponent<ParticleSystem>().Stop();
+			canvas.GetComponent<Canvas> ().enabled = false;
+			thankYou.GetComponent<Canvas> ().enabled = true;
+			yield return new WaitForSeconds(3);
+			Application.LoadLevel ("MainMenu");
 			break;
 		}
 		yield return null;
