019113b01afec4f58c8df9420397d814dd81d86c
Ben Wencke <benwencke@gmail.com>
2016-01-16 19:34:38 -0500

    Added item reactions and running

--- game/Assets/Scripts/TopLevelController.cs
+++ game/Assets/Scripts/TopLevelController.cs
@@ -12,4 +12,8 @@ public class TopLevelController : MonoBehaviour {
 	public virtual void TriggerPrimaryAction(System.Action<string> callback) {
 		Debug.Log ("Primary Action Triggered");
 	}
+
+	public virtual void KeyDown() {
+		Debug.Log ("clicked");
+	}
 }
