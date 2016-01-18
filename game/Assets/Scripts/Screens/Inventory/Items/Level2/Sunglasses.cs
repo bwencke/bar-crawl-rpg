37119bed83d253f8edbb4f6ba3495832e36a88d5
Ben Wencke <benwencke@gmail.com>
2016-01-18 02:11:11 -0500

    - transparency on inventory items - added all level 2 items - implemented basic version of disguise

---
+++ game/Assets/Scripts/Screens/Inventory/Items/Level2/Sunglasses.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Sunglasses : InventoryItemController {
+
+	public Image playerImage;
+
+	public override void UseItemOnSelf() {
+		ConversationController conversationController = GameObject.FindGameObjectWithTag ("Conversation").GetComponent<ConversationController> ();
+		conversationController.dialogueEngine.setVar ("WearingSunglasses", true);
+		GameObject.FindGameObjectWithTag ("SuccessChime").GetComponent<AudioObject> ().PlayAudio ();
+
+		if (conversationController.dialogueEngine.checkVar ("WearingMoustache")) {
+			GameObject.FindGameObjectWithTag ("Alert").GetComponent<AlertController> ().ShowStaticAlert("Congratulations! Your disguise is complete!");
+			playerImage.sprite = (Sprite) Resources.Load ("Images/BlakeDisguise", typeof(Sprite));
+		} else {
+			GameObject.FindGameObjectWithTag ("Alert").GetComponent<AlertController> ().ShowStaticAlert("You put the sunglasses on. Coolness: +200");
+			playerImage.sprite = (Sprite) Resources.Load ("Images/BlakeSunglasses", typeof(Sprite));
+		}
+
+		Disable ();
+	}
+}
